7dc4143841e274d839ae121187c4ad763ac25f28
silverrush2013 <silverrush2013@gmail.com>
2016-03-03 21:39:49 +0300

    hw

--- List2/main.cpp
+++ List2/main.cpp
@@ -7,6 +7,7 @@ struct Element
 {
     char b;
     Element *next=NULL;
+    Element *prev=NULL;
 };
 
 struct Linked_List
@@ -16,11 +17,74 @@ struct Linked_List
 };
 
 
-Linked_List *Create()
+void DeleteElement(Element **ch3,Element **beg,Element **end)
+{
+    if (*ch3==*beg)
+    {
+    *beg=(*beg)->next;
+    (*beg)->prev=0;}
+    else if(*ch3==*end)
+    {
+    *end=(*end)->prev;
+        (*end)->next=0;
+    }
+    else
+    {
+        (*ch3)->prev->next=(*ch3)->next;
+        (*ch3)->next->prev=(*ch3)->prev;
+    }
+    delete *ch3;
+}
+
+Linked_List *DeleteWord(Linked_List *word,Linked_List *text,int length2)
+{
+    int i=0;
+    int j=0;
+    Element *beg;
+    beg=text->first;
+    Element *end;
+    end=text->last;
+    Element *ch1;
+    ch1=text->first;//указатель в тексте на элемент слова(по всему тексту)
+    Element *ch2;
+    ch2=word->first;// указатель в слове
+    Element *ch3;//указатель на элемент слова,который совпадает с искомым
+    while (ch1!=NULL) {
+        ch3=ch1;
+        while ((ch1=ch2)&&(ch1!=NULL)) //пока не конец списка и символ является буквой
+        {
+            i++;
+            ch1=ch1->next;
+            ch2=ch2->next;
+            if (i==length2)
+            {
+
+                if (i>1) {
+                    while (j != length2) {
+                        DeleteElement(&ch3, &beg, &end);
+                        ch3 = ch3->next;
+                        j++;
+                    }
+                }
+                else
+                {
+                    DeleteElement(&ch3, &beg, &end);
+                }
+            }
+
+        }
+        i=0;
+        j=0;
+        ch2=word->first;
+        ch1=ch1->next;
+    }
+    return text;
+}
+
+
+Linked_List *Create(string wrd)
 {
     Linked_List *Words= new Linked_List;
-    string wrd;
-    getline(cin,wrd);
     for (int i=0; i<(wrd.length());i++ )
     {
         if (Words->first==NULL)
@@ -44,12 +108,21 @@ Linked_List *Create()
 }
 
 int main() {
-    Linked_List *list=Create();
-    int length=0;
-    Element *str =list->first;
-    while (str != NULL) //вывод на экран списка
-    {
-        cout <<str->b;
-        str = str->next;
-    }
+    string txt;
+    string wrd;
+    int length2;
+    cout <<"Enter the text"<<endl;
+    getline(cin,txt);
+    Linked_List *text=Create(txt);
+    cout <<"Enter the word" << endl;
+    getline(cin,wrd);
+    length2=wrd.length();
+    Linked_List *word=Create(wrd);
+   DeleteWord(word,text,length2);
+    Element *str =text->first;
+        while (str != NULL) //вывод на экран списка
+        {
+            cout << str->b;
+            str = str->next;
+        }
 }
